e7b88b1d54f896608b71d14aa0be1ceaa4a7c489
shaheem <shaheemmahmood728@gmail.com>
2025-04-02 22:01:35 +0530

    Added GEN AI access keys and secret keys to settings.py and implemented AI-powered resume extraction in utils.py and views.py

--- resume_checker/views.py
+++ resume_checker/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .validators import validate_file_extension,validate_file_size
 from django.core.files.uploadedfile import UploadedFile
 from django.core.exceptions import ValidationError
-from .utils import read_uploaded_file
+from .utils import read_uploaded_file,extract_resume_info
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
@@ -16,7 +16,8 @@ def upload_resume(request):
             validate_file_extension(file)
             validate_file_size(file)
             file_content = read_uploaded_file(file , ext)
+            data = extract_resume_info(file_content)
             print("file=",file_content)
         except ValidationError as err: 
-            return JsonResponse({"error": str(err.message)})
+            return JsonResponse({"error": str(err.message)},status=400)
     return JsonResponse({"error":"Invalid Request"},status=400)    
